6dfb6132f50e2bae9eeb91422827cf20a407aa1d
Rimon <rimons2804@gmail.com>
2022-07-20 19:04:32 +0300

    store chagnes

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -143,22 +143,6 @@ function _createBoard() {
           {
             id: _makeId(),
             title: 'new card2',
-<<<<<<< HEAD
-            labelIds:['l101','l102'],
-            checklists: [
-              {
-                  id: 'YEhmF',
-                  title: 'Checklist',
-                  todos: [
-                      {
-                          id: '212jX',
-                          title: 'To Do 1',
-                          isDone: false
-                      }
-                  ]
-              }
-          ],
-=======
             labelIds: ['l101', 'l102'],
           },
         ],
@@ -200,7 +184,6 @@ function _createBoard() {
             id: _makeId(),
             title: 'new card2',
             labelIds: ['l101', 'l102'],
->>>>>>> 1085d1a79d3cb405fc24d445e25eb7ea7186b9d3
           },
         ],
       },
