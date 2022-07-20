88a6f4247d3201d10ce34a33fc9f6034befc18fb
stav kotlovsky <stavkot1999@gmail.com>
2022-07-20 23:04:52 +0300

    board list

--- src/views/board.list.vue
+++ src/views/board.list.vue
@@ -1,10 +1,15 @@
 <template>
   <section class="board-list-container" v-if="boards">
     <h1>Most popular templates</h1>
-    <div class="board-list" v-for="board in boards" :key="board._id">
-      {{ board._id }}
-      {{ board.title }}
-      <router-link :to="'/board/' + board._id">Go to board</router-link>
+    <div class="board-list">
+      <div class="board-preview" v-for="board in boards" :key="board._id">
+        <ul>
+          <li>
+            {{ board.title }}
+            <router-link :to="'/board/' + board._id">Go to board</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
