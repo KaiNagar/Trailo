62b98a87102eb49373aeaf2c5ff1f3a5daaa2926
Kai Nagar <kaynagar@gmail.com>
2022-07-21 19:29:14 +0300

    finishing cardDetail scss kai

--- src/cmps/card/action.checklist.vue
+++ src/cmps/card/action.checklist.vue
@@ -1,22 +1,34 @@
 <template>
   <section class="card-checklist">
-    <h1>{{ checklist.title }}</h1>
-    <div>0%</div>
-
-    <div v-for="(todo, idx) in checklist.todos" :key="idx">
-      <input @change="saveChecklist" v-model="todo.isDone" type="checkbox" />
-      <input @change="saveChecklist" type="text" :value="todo.title" />
-      <button @click="removeTodo(idx)">X</button>
+    <div class="checklist-icon">
+      <img
+        src="https://cdn-icons.flaticon.com/png/512/2440/premium/2440972.png?token=exp=1658390307~hmac=3f013390315ecbdaec4f9d1514b8ec42"
+        alt="Checklist icon"
+      />
     </div>
-    <!-- <button v-show="!isEditTitle" @click="onAddItem">Add an item</button> -->
+    <div class="checklist-action">
+      <div class="checklist-header">
+        <h3>{{ checklist.title }}</h3>
+        <button class="delete-checklist-btn">Delete</button>
+      </div>
+      <div>0%</div>
 
-    <!-- <div v-show="isEditTitle" class="checklist-todo"> -->
-    <form class="flex column" @submit.prevent="saveChecklist">
-      <input ref="newItemInput" type="text" placeholder="Add an item" />
-      <!-- <button @click="addTodo">Add</button> -->
-    </form>
-    <!-- <button @click="isEditTitle = false">Cancel</button> -->
-    <!-- </div> -->
+      <div v-for="(todo, idx) in checklist.todos" :key="idx">
+        <input @change="saveChecklist" v-model="todo.isDone" type="checkbox" />
+        <input @change="updateTodo" type="text" :value="todo.title" />
+        <button @click="removeTodo(idx)">X</button>
+      </div>
+      
+      <!-- <button v-show="!isEditTitle" @click="onAddItem">Add an item</button> -->
+      <!-- <div v-show="isEditTitle" class="checklist-todo"> -->
+        
+      <form class="flex column" @submit.prevent="saveChecklist">
+        <input ref="newItemInput" type="text" placeholder="Add an item" />
+        <!-- <button @click="addTodo">Add</button> -->
+      </form>
+      <!-- <button @click="isEditTitle = false">Cancel</button> -->
+      <!-- </div> -->
+    </div>
   </section>
 </template>
 
@@ -36,7 +48,7 @@ export default {
   methods: {
     saveChecklist() {
       this.newItem.title = this.$refs.newItemInput.value
-      this.checklist.todos.push(this.newItem)
+      this.checklist.todos.push(JSON.parse(JSON.stringify(this.newItem)))
 
       this.$emit('saveChecklist', {
         info: { checklist: this.checklist, idx: this.idx },
@@ -45,6 +57,11 @@ export default {
     },
     removeTodo(todoIdx) {
       this.checklist.todos.splice(todoIdx, 1)
+      this.$emit('saveChecklist', {
+        info: { checklist: this.checklist, idx: this.idx },
+      })
+    },
+    updateTodo() {
       this.$emit('saveChecklist', {
         info: { checklist: this.checklist, idx: this.idx },
       })
--- src/cmps/card/action.description.vue
+++ src/cmps/card/action.description.vue
@@ -1,6 +1,25 @@
 <template>
-  <section>
-    <h1>this is description  cmp</h1>
+  <section class="card-description">
+    <div>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/54/54586.png"
+        alt="Description icon"
+      />
+    </div>
+    <div class="desc-textarea">
+      <h2>Description</h2>
+      <div v-if="!editing" @click="editing = true" class="desc-body-container">
+        <div class="desc-body">Add a more detailed description...</div>
+      </div>
+      <div v-else class="edit-desc">
+        <textarea
+          v-model="description"
+          placeholder="Add a more detailed description..."
+        ></textarea>
+        <button @click="editDesc">Save</button>
+        <button @click="editing = false">Cancel</button>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -9,9 +28,16 @@ export default {
   name: 'description',
   components: {},
   data() {
-    return {}
+    return {
+      editing: false,
+      description: '',
+    }
   },
-  methods: {},
+  methods: {
+    editDesc() {
+      this.editing = false
+    },
+  },
   computed: {},
   created() {},
 }
