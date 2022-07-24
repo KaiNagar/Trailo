79341e14ddd4a60175a2f3078c03638b7fabbbe3
stav kotlovsky <stavkot1999@gmail.com>
2022-07-24 22:13:56 +0300

    commit to pull

--- src/views/login.page.vue
+++ src/views/login.page.vue
@@ -2,20 +2,20 @@
   <section>
     <form>
       <h1>this is login form</h1>
-      <input type="text" placeholder="put something inside me plaese">
+      <input type="text" placeholder="put something inside me plaese" />
     </form>
   </section>
 </template>
 
 <script>
 export default {
-  name:'Login',
-components:{},
-data() {
-return {};
-},
-methods: {},
-computed: {},
-created() {},
-};
+  name: 'Login',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  created() {},
+}
 </script>
