bf26b1730cb7c2c4d2729463cb6c7d2b5cba4886
Stav Kotlovsky <stavkot1999@gmail.com>
2022-07-26 23:37:15 +0300

    date component

--- src/views/user.details.vue
+++ src/views/user.details.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <section>
     <h1>User details</h1>
   </section>
@@ -17,27 +17,4 @@ export default {
 }
 </script>
 
-<style></style> -->
-<template>
-  <v-calendar ref="calendar">
-    <template v-slot:footer>
-      <div class="bg-gray-100 text-center p-2 border-t rounded-b-lg">
-        <button
-          class="bg-blue-500 text-white font-medium px-2 py-1 rounded hover:bg-blue-600"
-          @click="moveToToday"
-        >
-          Today
-        </button>
-      </div>
-    </template>
-  </v-calendar>
-</template>
-  <script>
-export default {
-  methods: {
-    moveToToday() {
-      this.$refs.calendar.move(new Date())
-    },
-  },
-}
-</script>
+<style></style>
