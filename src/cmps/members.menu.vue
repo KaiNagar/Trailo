a486cc914ceb5a65135ee045c03358543b9eefa1
Kai Nagar <kaynagar@gmail.com>
2022-07-26 14:38:28 +0300

    added members menu

---
+++ src/cmps/members.menu.vue
@@ -0,0 +1,77 @@
+<template>
+  <app-modal>
+    <template #title>Members</template>
+    <template #part-1>
+      <input class="members-input" type="text" placeholder="Search members" />
+    </template>
+    <template #part-2>
+      <h1 class="header">Board members</h1>
+      <div
+        class="member-tab flex align-center space-between"
+        v-for="member in members"
+        :key="member.id"
+      >
+        <div class="flex align-center ">
+          <div
+            class="member-img"
+            v-if="member.imgUrl"
+            :style="memberImg(member)"
+          ></div>
+          <div
+            :style="{ backgroundColor: member.color }"
+            class="member-cred flex align-center"
+            v-else
+          >
+            {{ memberCred(member) }}
+          </div>
+          <span class="formatted-name"
+            >{{ formatedName(member.fullname)
+            }}<span class="short-name">({{ member.fullname }})</span>
+          </span>
+        </div>
+        <span v-if="member?.isSelected" class="check-icon"></span>
+      </div>
+    </template>
+    <template #part-3
+      ><button class="more-members btn">
+        Show other Workspace members
+      </button></template
+    >
+  </app-modal>
+</template>
+
+<script>
+import appModal from './app.modal.vue'
+export default {
+  props: {
+    members: Array,
+  },
+  components: { appModal },
+  data() {
+    return {}
+  },
+  methods: {
+    formatedName(name) {
+      let formatedName = name.split(' ')
+      const firstName =
+        formatedName[0].charAt(0).toUpperCase() + formatedName[0].slice(1)
+      const lastName =
+        formatedName[1].charAt(0).toUpperCase() + formatedName[1].slice(1)
+      return firstName + ' ' + lastName
+    },
+    memberImg(member) {
+      if (member.imgUrl)
+        return { backgroundImage: 'url(' + member.imgUrl + ')' }
+    },
+    memberCred(member) {
+      const firstL = member.fullname.split(' ')[0].charAt(0).toUpperCase()
+      const lastL = member.fullname.split(' ')[1].charAt(0).toUpperCase()
+      return firstL + lastL
+    },
+  },
+  computed: {},
+  created() {},
+}
+</script>
+
+<style></style>
--- src/views/login.page.vue
+++ src/views/login.page.vue
@@ -4,18 +4,59 @@
       <h1>this is login form</h1>
       <input type="text" placeholder="put something inside me plaese" />
     </form>
+    <members-menu :members="members" />
   </section>
 </template>
 
 <script>
+import membersMenu from '../cmps/members.menu.vue'
 export default {
   name: 'Login',
-  components: {},
+  components: { membersMenu },
   data() {
     return {}
   },
   methods: {},
-  computed: {},
+  computed: {
+    members() {
+      return [
+        {
+          _id: 'u101',
+          fullname: 'kagol mokakor',
+          email: 'kagol@gmail.com',
+          imgUrl: '',
+                      color:this.getRandomColor
+
+        },
+        {
+          _id: 'u102',
+          fullname: 'manoka menika',
+          email: 'manoka@gmail.com',
+          imgUrl:
+            '',
+                        color:this.getRandomColor
+
+        },
+        {
+          _id: 'u103',
+          fullname: 'menashe moko',
+          email: 'menashe@gmail.com',
+          imgUrl:
+            '',
+            color:this.getRandomColor
+        },
+      ]
+    },
+    getRandomColor() {
+      var letters = '0123456789ABCDEF'
+      var color = '#'
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+
+      return color
+    }
+  },
+  },
   created() {},
 }
 </script>
