1795e97ecce4b5ca3eeee0c6dd48d54307275f18
Rimon <rimons2804@gmail.com>
2022-07-29 22:58:16 +0300

    members modal and functionality 0.8

--- src/cmps/board/board.header.vue
+++ src/cmps/board/board.header.vue
@@ -2,29 +2,16 @@
   <section class="board-header full flex space-between">
     <nav>
       <button class="board-btn">
-        <span class="bars"
-          ><img src="../../assets/icons/icons-bar-chart.png" alt=""
-        /></span>
+        <span class="bars"><img src="../../assets/icons/icons-bar-chart.png" alt="" /></span>
         <span class="txt">Board</span>
-        <span class="img"
-          ><img src="../../assets/icons/icons-down.png" alt=""
-        /></span>
+        <span class="img"><img src="../../assets/icons/icons-down.png" alt="" /></span>
       </button>
-      <h1
-        v-if="!editBoardTitle"
-        class="board-header-title"
-        @click="openEditTitle"
-      >
+
+      <h1 v-if="!editBoardTitle" class="board-header-title" @click="openEditTitle">
         {{ currBoard.title }}
       </h1>
-      <input
-        ref="editBoardRef"
-        @blur="saveBoardTitle"
-        v-else
-        type="text"
-        v-model="currBoard.title"
-        class="edit-board-title"
-      />
+      <input ref="editBoardRef" @blur="saveBoardTitle" v-else type="text" v-model="currBoard.title"
+        class="edit-board-title" />
 
       <button @click="toggleStarBoard" class="board-header-star">
         <span :style="isStarred" class="star-icon"></span>
@@ -34,47 +21,34 @@
         <span class="privet-icon"></span> Private
       </button>
     </nav>
+
+    <!-- MEMBERS -->
+    <avatar-list @users="users"></avatar-list>
+    <button @click="isShareBoard = true">share</button>
+    <share-board v-if="isShareBoard" @close="isShareBoard = false"></share-board>
+
+
     <div class="board-header-right-btns">
       <button class="filter-btn">
-        <span class="img"
-          ><img src="../../assets/icons/icons-bars.png" alt=""
-        /></span>
+        <span class="img"><img src="../../assets/icons/icons-bars.png" alt="" /></span>
         <span class="txt">Filter</span>
       </button>
       <board-filter v-if="filterMenu" />
+
       <div>
-        <button
-          v-if="!isMoreMenu"
-          @click="showMenu"
-          class="show-menu-btn flex align-center"
-        >
+        <button v-if="!isMoreMenu" @click="showMenu" class="show-menu-btn flex align-center">
           <span class="menu-icon"></span>
           <span class="txt">Show menu</span>
         </button>
-        <show-more
-          @openStyleMenu="openStyleMenu"
-          @closeMoreMenu="hideMenu"
-          :style="setMenuMore"
-        />
-        <style-menu
-          @backToMenu="backToMenuFromStyle"
-          @openColorsMenu="openColorsMenu"
-          @openImgsMenu="openImgsMenu"
-          @closeMoreMenu="hideMenu"
-          :style="setMenuStyle"
-        />
-        <color-menu
-          @backToStyle="backToStyleFromColors"
-          @setBgColor="setBgColor"
-          :style="setMenuColors"
-          @closeMoreMenu="hideMenu"
-        />
-        <photo-menu
-          @closeMoreMenu="hideMenu"
-          @backToStyle="backToStyleFromImgs"
-          @setBgImg="setBgImg"
-          :style="setMenuPhotos"
-        />
+        <show-more @openStyleMenu="openStyleMenu" @closeMoreMenu="hideMenu" :style="setMenuMore" />
+        <style-menu @backToMenu="backToMenuFromStyle" @openColorsMenu="openColorsMenu" @openImgsMenu="openImgsMenu"
+          @closeMoreMenu="hideMenu" :style="setMenuStyle" />
+        <color-menu @backToStyle="backToStyleFromColors" @setBgColor="setBgColor" :style="setMenuColors"
+          @closeMoreMenu="hideMenu" />
+        <photo-menu @closeMoreMenu="hideMenu" @backToStyle="backToStyleFromImgs" @setBgImg="setBgImg"
+          :style="setMenuPhotos" />
+
+
       </div>
     </div>
   </section>
@@ -86,10 +60,15 @@ import showMore from '../menu/show.more.vue'
 import styleMenu from '../menu/style.menu.vue'
 import colorMenu from '../menu/color.menu.vue'
 import photoMenu from '../menu/photo.menu.vue'
+import membersList from '../members.list.vue'
+import avatarList from '../card/avatar.list.vue'
+import menuMembers from '../menu/menu.members.vue'
+import shareBoard from '../user/share.board.vue'
+
 
 export default {
   name: 'boardHeader',
-  components: { boardFilter, showMore, styleMenu, photoMenu, colorMenu },
+  components: { boardFilter, shareBoard, menuMembers, avatarList, showMore, styleMenu, photoMenu, colorMenu, membersList },
   data() {
     return {
       filterMenu: false,
@@ -99,20 +78,21 @@ export default {
       isStyleMenu: false,
       isPhotosMenu: true,
       isColorsMenu: false,
+      isShareBoard:false,
     }
   },
   methods: {
-    setBgImg(img){
+    setBgImg(img) {
       const board = JSON.parse(JSON.stringify(this.stateBoard))
       board.style.bgColor = null
       board.style.bgImg = img.url
-      this.$store.dispatch({type:'saveBoard',board})
+      this.$store.dispatch({ type: 'saveBoard', board })
     },
     setBgColor(color) {
       const board = JSON.parse(JSON.stringify(this.stateBoard))
       board.style.bgImg = null
       board.style.bgColor = color.color
-      this.$store.dispatch({type:'saveBoard',board})
+      this.$store.dispatch({ type: 'saveBoard', board })
     },
     backToStyleFromImgs() {
       this.isStyleMenu = true
@@ -163,6 +143,9 @@ export default {
       updatedBoard.isStarred = !updatedBoard.isStarred
       this.$store.dispatch({ type: 'saveBoard', board: updatedBoard })
     },
+    openMenu(menuAction) {
+      this.$store.commit({ type: 'openMenu', menuAction })
+    },
   },
   computed: {
     setMenuPhotos() {
@@ -190,6 +173,10 @@ export default {
     stateBoard() {
       return this.$store.getters.currBoard
     },
+    menu() {
+      return this.$store.getters.menu
+    },
+
   },
   created() {
     this.currBoard = JSON.parse(JSON.stringify(this.stateBoard))
@@ -198,4 +185,5 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+</style>
---
+++ src/cmps/user/search.user.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="search-user">
+        <ul>
+
+            <li v-for="user in users" :key="user._id" @click="addMember(user)">
+                <div class="icon avatar">{{ user.username.slice(0, 1).toUpperCase() }}</div>
+                <div class="content">
+                    <div class="top">{{ user.username }}</div>
+                    <div class="bottom">Hasn't logged in recently</div>
+                </div>
+            </li>
+        </ul>
+    </section>
+
+</template>
+ <script>
+export default {
+    name: 'ProjectApp',
+    props: {
+
+    },
+    components: {},
+    data() {
+        return {
+            userSearch: '',
+            
+        };
+    },
+    created() {
+
+    },
+    methods: {
+        addMember(user) {
+            console.log(this.board.members);
+            const newBoard = JSON.parse(JSON.stringify(this.board))
+            const updatedUser = JSON.parse(JSON.stringify(user))
+            updatedUser.isAdmin = false
+            if (!newBoard.members.length || !newBoard.members) newBoard.members = []
+            newBoard.members.push(updatedUser)
+            this.$store.dispatch({ type: 'saveBoard', board: newBoard })
+        },
+    },
+    computed: {
+        users() {
+            return this.$store.getters.filteredUsers || this.$store.getters.users
+        },
+        board() {
+            return this.$store.getters.currBoard
+        }
+    },
+    unmounted() { },
+
+};
+</script>
+ <style>
+ </style>
